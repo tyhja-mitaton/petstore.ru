701a4571725a0943909afbd4db4a63b2243b10df
tyhja-mitaton <nullum0@gmail.com>
2019-09-26 12:34:57 +0500

    fix

--- api/modules/v1/models/Pet.php
+++ api/modules/v1/models/Pet.php
@@ -22,29 +22,4 @@ class Pet extends ActiveRecord
         return '{{%pet}}';
     }
 
-    private function getPhone()
-    {
-        $identity = \api\modules\v1\models\User::findOne(['id' => $this->user_id]);
-        return $identity->phone;
-    }
-
-    private function getCategory()
-    {
-        $identity = \api\modules\v1\models\Category::findOne(['id' => $this->category_id]);
-        return $identity->title;
-    }
-
-    public function fields()
-    {
-        $fields = parent::fields();
-        unset($fields['category_id'], $fields['user_id']);
-        $fields['phone'] = function () {
-            return $this->getPhone();
-        };
-        $fields['category'] = function () {
-            return $this->getCategory();
-        };
-        return $fields;
-    }
-
 }
